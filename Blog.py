365dbae9454b0974fd3fafcae12131013046e1f5
Mush <mushtaqbasha7.mb@gmail.com>
2024-03-13 11:54:42 +0530

    Blogs
    Automation testing for Opening blogs in Pinterest

---
+++ Blog.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+print("Getting started")
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(3000)
+driver.get("https://www.pinterest.com/")
+search_box = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[1]/div/div/div/div/div[1]/div/div[2]/div[1]/div[3]/div/a')
+search_box.click()
+time.sleep(30)
+print("Closing")
