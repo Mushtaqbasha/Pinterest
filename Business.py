de44855d4e1fbfcf37275d7e9a11b23f393e0875
Mush <mushtaqbasha7.mb@gmail.com>
2024-03-14 09:17:45 +0530

    Business page in Pinterest
    Reporting Business site in Pinterest

--- Business.py
+++ Business.py
@@ -1,13 +1,19 @@
-import time
 from selenium import webdriver
-from selenium.webdriver import Keys
+import allure
+import pytest
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-print("Getting started")
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.implicitly_wait(3000)
-driver.get("https:/www.pinterest.com/")
-search_box = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[1]/div/div/div/div/div[1]/div/div[2]/div[1]/div[2]')
-search_box.click()
-time.sleep(30)
-print("Closing")
+import time
+@allure.severity(allure.severity_level.NORMAL)
+class TestPinterest:
+    @allure.severity(allure.severity_level.MINOR)
+    def test_Google_Search(self):
+        print("Getting started")
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(30)
+        self.driver.get("https:/www.pinterest.com/")
+        search_box = self.driver.find_element(By.XPATH,'/html/body/div[1]/div/div[1]/div/div/div/div/div[1]/div/div[2]/div[1]/div[2]')
+        search_box.click()
+        time.sleep(3)
+        print("Closing")
