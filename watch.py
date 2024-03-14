abebe833de70f9e06e47417dc8a9236329f43882
Mush <mushtaqbasha7.mb@gmail.com>
2024-03-14 09:37:19 +0530

    Watch Page
    REporting Watch Page in Pineterest

--- watch.py
+++ watch.py
@@ -1,13 +1,20 @@
-import time
 from selenium import webdriver
+import allure
+import pytest
 from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-print("Getting started")
-driver=webdriver.Chrome()
-driver.maximize_window()
-driver.implicitly_wait(3000)
-driver.get("https://in.pinterest.com/videos/")
-search_box = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[1]/div/div/div/div/div[1]/div/div[1]/div[2]/div[3]/div/a/div/div')
-search_box.click()
-time.sleep(30)
-print("Closing")
+import time
+@allure.severity(allure.severity_level.NORMAL)
+class TestPinterest:
+    @allure.severity(allure.severity_level.MINOR)
+    def test_Google_Search(self):
+        print("Getting started")
+        self.driver=webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(30)
+        self.driver.get("https://in.pinterest.com/videos/")
+        search_box = self.driver.find_element(By.XPATH,'/html/body/div[1]/div/div[1]/div/div/div/div/div[1]/div/div[1]/div[2]/div[3]/div/a/div/div')
+        search_box.click()
+        time.sleep(3)
+        print("Closing")
