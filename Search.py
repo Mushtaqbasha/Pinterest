c46ef928a10cb180c7c43edf9800cf94bbc79992
Mush <mushtaqbasha7.mb@gmail.com>
2024-03-14 09:27:44 +0530

    Search page
    Reporting Search page

--- Search.py
+++ Search.py
@@ -1,14 +1,21 @@
-import time
 from selenium import webdriver
+import allure
+import pytest
 from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-print("Getting started")
-driver=webdriver.Chrome()
-driver.maximize_window()
-driver.implicitly_wait(3000)
-driver.get("https://www.pinterest.com/ideas/")
-search_box = driver.find_element("xpath",'//*[@id="searchBoxContainer"]/div/div/div[2]/input')
-search_box.send_keys("onepiece")
-search_box.send_keys(Keys.ENTER)
-time.sleep(30)
-print("Closing")
+import time
+@allure.severity(allure.severity_level.NORMAL)
+class TestPinterest:
+    @allure.severity(allure.severity_level.MINOR)
+    def test_Google_Search(self):
+        print("Getting started")
+        self.driver=webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(3)
+        self.driver.get("https://www.pinterest.com/ideas/")
+        search_box = self.driver.find_element("xpath",'//*[@id="searchBoxContainer"]/div/div/div[2]/input')
+        search_box.send_keys("onepiece")
+        search_box.send_keys(Keys.ENTER)
+        time.sleep(3    )
+        print("Closing")
